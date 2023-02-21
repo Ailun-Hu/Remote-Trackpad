75ee2152f66d97c753198e460680a30cf2db1349
Alan Hu <64699846+Ailun-Hu@users.noreply.github.com>
2023-02-21 17:24:20 -0500

    deleted backend folder

--- Desktop-Executable/Program.cs
+++ Desktop-Executable/Program.cs
@@ -1,40 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Net.WebSockets;
-using System.Buffers;
-using System.Text.Encodings;
-
 
 namespace MoveMouse
 {
     class Program{
 
         static void Main(string[] args){
-            
             ListenAndMove();  
-            Console.WriteLine("Hello");
         }
 
         static async void ListenAndMove()
         {
             // int x = 0;
             // int y = 0;
-            Console.WriteLine("Listen");
             var result = new byte[1024];
             Uri link = new Uri("ws://localhost:8080");
-            ClientWebSocket ws = new ClientWebSocket();
+            using(ClientWebSocket ws = new ClientWebSocket()){
             Console.WriteLine(link.ToString());
 
             ws.ConnectAsync(link, CancellationToken.None).Wait();
             Console.WriteLine(ws.Options.KeepAliveInterval);
-            Console.WriteLine("Connected");
             while (ws.State == WebSocketState.Open)
             {
-                Console.WriteLine("IN Loop");
-                ws.ReceiveAsync(new ArraySegment<byte>(result), CancellationToken.None).Wait();
-                Console.WriteLine(System.Text.Encoding.Default.GetString(result));
-                Console.WriteLine("Test");
+                WebSocketReceiveResult res = ws.ReceiveAsync(new ArraySegment<byte>(result), CancellationToken.None).WaitAsync(TimeSpan.FromMinutes(1)).Result;
+                var data = System.Text.Encoding.UTF8.GetString(new ArraySegment<byte>(result, 0, res.Count));
+                var list = data.Split(' ');
+                Console.WriteLine(list[0] + ' ' + list[1]);
             }
-            Console.WriteLine("Ended");
+            ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed", CancellationToken.None).Wait();
+            }
         }
     }
 }
