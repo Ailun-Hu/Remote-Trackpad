9732346b908d3a390960a8fc4d49055233d1d7b1
Ailun Hu <ahu7485@gmail.com>
2023-02-21 20:08:22 -0500

    updated c# file

--- Desktop-Executable/Program.cs
+++ Desktop-Executable/Program.cs
@@ -2,34 +2,58 @@
 using System.Net.WebSockets;
 using System.Net.Sockets;
 using System.Net.NetworkInformation;
+using ArpLookup;
+using System.Net;
+using System.Windows.
 
 namespace MoveMouse
 {
     class Program{
-
+        
         static void Main(string[] args){
-        foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
-            {
-                Console.WriteLine("Name: " + netInterface.Name);
-                Console.WriteLine("Description: " + netInterface.Description);
-                Console.WriteLine("Addresses: ");
-                IPInterfaceProperties ipProps = netInterface.GetIPProperties();
-                foreach (UnicastIPAddressInformation addr in ipProps.UnicastAddresses)
-                {
-                Console.WriteLine(" " + addr.Address.ToString());
-                }
-                Console.WriteLine("");
-            }    
+            // while(true){
+            // string ipbase = "192.168.1.";
+            // for(int i = 1; i < 255; i++){
+            //     string ip = ipbase + i.ToString();
+            //     bool serverFound = pingCheck(ip, 50, 4201);
+            //     if(serverFound){
+            //         Console.WriteLine("Listened");
+            //         ListenAndMove(ip);
+            //     }
+
+            // } 
+            // }                 
+            ListenAndMove("192.168.1.121");  
+        }
+        static bool pingCheck(string host, int timeout, int port){
             
-            //ListenAndMove();  
+            System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
+            System.Net.NetworkInformation.PingReply pingReply;
+
+            try{
+                pingReply = ping.Send(host, timeout);
+                if(pingReply != null && pingReply.Status == System.Net.NetworkInformation.IPStatus.Success)
+                    using (TcpClient tcpClient = new TcpClient()){
+                        try{
+                            tcpClient.Connect(host, port);
+                            return true;
+                        }catch{
+                            return false;
+                        }
+                    }
+            }catch{
+                return false;
+            }
+            return false;
         }
 
-        static async void ListenAndMove()
+        static async void ListenAndMove(string ip)
         {
             // int x = 0;
             // int y = 0;
             var result = new byte[1024];
-            Uri link = new Uri("ws://192.168.1.121:4201");
+            string url = "ws://" + ip + ":4201";
+            Uri link = new Uri(url);
             using(ClientWebSocket ws = new ClientWebSocket()){
             Console.WriteLine(link.ToString());
 
@@ -44,6 +68,16 @@ namespace MoveMouse
             }
             ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed", CancellationToken.None).Wait();
             }
+        }
+
+        static void MoveCursor()
+        {
+        // Set the Current cursor, move the cursor's Position,
+        // and set its clipping rectangle to the form. 
+
+        this.Cursor = new Cursor(Cursor.Current.Handle);
+        Cursor.Position = new Point(Cursor.Position.X - 50, Cursor.Position.Y - 50);
+        Cursor.Clip = new Rectangle(this.Location, this.Size);
         }
     }
 }
